040abc3e5ad54788d969a2babecae0c39969616f
Cory G Watson <gphat@cpan.org>
2012-01-19 07:55:52 -0600

    More tests for Minion and Worker stuff.

--- src/test/java/test/SimpleMinion.java
+++ src/test/java/test/SimpleMinion.java
@@ -1,5 +1,6 @@
 package test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.anyMapOf;
@@ -67,19 +68,70 @@ public class SimpleMinion {
 		}
 	}
 
-	//@Test
-	public void testNoResults() {
+	@Test
+	/**
+	 * Tests that the worker "skips" valid JSON messages with missing
+	 * response_queue by confirming them without put()ing anything back into
+	 * kestrel.
+	 */
+	public void testMissingResponseQueue() {
 
 		String queueName = "foobar";
 
 		Client mockClient = mock(Client.class);
 		try {
-			when(mockClient.get(anyString(), anyInt())).thenReturn("first");
+			// Mock up the queue to return invalid JSON
+			when(mockClient.get(anyString(), anyInt())).thenReturn("{\"foo\":\"bar\"}");
 
+			// Mock up confirm to throw an RTE so that execution stops.
+			doThrow(new RuntimeException()).when(mockClient).confirm(anyString(), anyInt());
+
+		} catch(Exception e) {
+			e.printStackTrace();
+			fail("Failed to stub get method for kestrel client");
+		}
+
+		PerformanceCollector pc = new PerformanceCollector("foo", false);
+
+		// Mock up an empty worker
+		Worker mockWorker = mock(Worker.class);
+
+		Minion m = new Minion(pc, mockClient, queueName, mockWorker, 1000);
+		try {
+			m.run();
+		} catch(Exception e) {
+			m.cancel();
+		}
+
+		// Verify that put was NEVER called.
+		try {
+			verify(mockClient, never()).put(anyString(), anyString());
+		} catch(Exception e) {
+			fail("Got weird exception from mock put. Wtf?");
+		}
+	}
+
+	@Test
+	/**
+	 * Tests that the Minion properly inserts a single EOF message in response
+	 * to worker results that have NO entries in their iterator.
+	 *
+	 * This test verifies that things like INSERT statements – which have nothing
+	 * to return – get an appropriate confirmation back.
+	 */
+	public void testNoResults() {
+
+		Client mockClient = mock(Client.class);
+		try {
+			// Mock the basic JSON stuff in the queue.
+			when(mockClient.get(anyString(), anyInt())).thenReturn("{\"response_queue\":\"whatever\"}");
+
+			// Verify that we ONLY get the EOF and throw an Exception to stop
 			doAnswer(new Answer() {
 				public Object answer(InvocationOnMock invocation) {
-					System.out.println("ASDASDDS");
-					return null;
+					Object[] args = invocation.getArguments();
+					assertEquals("EOF", "{ \"EOF\":\"EOF\" }", args[1]);
+					throw new RuntimeException();
 				}
 			}).when(mockClient).put(anyString(), anyString());
 		} catch(Exception e) {
@@ -91,7 +143,11 @@ public class SimpleMinion {
 
 		Worker mockWorker = mock(Worker.class);
 		try {
-			when(mockWorker.handle((PerformanceCollector) anyObject(), (HashMap) anyMapOf(String.class, Object.class))).thenReturn(
+			// Mock up the handle method to return an empty iterator.
+			when(mockWorker.handle(
+					(PerformanceCollector) anyObject(),
+					(HashMap) anyMapOf(String.class, Object.class))
+			).thenReturn(
 				new ArrayList<String>().iterator()
 			);
 		} catch(Exception e) {
@@ -99,7 +155,11 @@ public class SimpleMinion {
 			fail("Failed to stub handle method for worker");
 		}
 
-		Minion m = new Minion(pc, mockClient, queueName, mockWorker, 1000);
-		m.run();
+		Minion m = new Minion(pc, mockClient, "foobar", mockWorker, 1000);
+		try {
+			m.run();
+		} catch(Exception e) {
+			m.cancel();
+		}
 	}
 }
